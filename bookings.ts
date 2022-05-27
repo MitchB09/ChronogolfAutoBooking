07aa07f1f33c3af65e5563c644881d6a9a31aa18
Mitch Bilensky <mitchbilensky@gmail.com>
2022-05-26 23:37:42 -0300

    2022-05-26

--- bookings.ts
+++ bookings.ts
@@ -1,19 +1,40 @@
 interface BookingData {
-    clubId: number;
-    month: string;
-    day: number;
-    time: string;
-    numPlayers: number;
+  clubId: number;
+  month: string;
+  day: number;
+  time: string;
+  numPlayers: number;
 }
 
 const bookingData: BookingData[] = [
-    {
-        clubId: 18848,
-        month: 'May',
-        day: 19,
-        time: '4:30 PM',
-        numPlayers: 3
-    }
+  {
+    clubId: 18848,
+    month: "May",
+    day: 19,
+    time: "4:30 PM",
+    numPlayers: 3,
+  },
+  {
+    clubId: 18848,
+    month: "May",
+    day: 29,
+    time: "4:40 PM",
+    numPlayers: 3,
+  },
+  {
+    clubId: 18848,
+    month: "May",
+    day: 31,
+    time: "4:30 PM",
+    numPlayers: 3,
+  },
+  {
+    clubId: 18848,
+    month: "June",
+    day: 2,
+    time: "4:30 PM",
+    numPlayers: 3,
+  },
 ];
 
 export default bookingData;
--- cypress/integration/specs/bookTeeTime.spec.ts
+++ cypress/integration/specs/bookTeeTime.spec.ts
@@ -1,31 +1,85 @@
-import { BookingPage } from '../../pages/booking';
-import { LoginPage } from '../../pages/login';
-import bookings from '../../../bookings'; 
+import { BookingPage } from "../../pages/booking";
+import { LoginPage } from "../../pages/login";
+import bookings from "../../../bookings";
 
 const login = new LoginPage();
 const bookingPage = new BookingPage();
+const addDays = function (date: Date, days: number) {
+  var newDate = new Date(date.valueOf());
+  newDate.setDate(newDate.getDate() + days);
+  return newDate;
+};
 
-describe('Book A Tee Time', () => {
-    
-    bookings.forEach(booking => {
-        it(`Book TeeTime for ${booking.time} on ${booking.month} ${booking.day} `,() => {
-            cy.visit("");
-            cy.intercept('GET', '/private_api/sessions').as('loginSuccess');
-            login.login();
-            cy.wait('@loginSuccess');
+const sleepUntilBookngTime = () => {
+  const bookingTime = new Date().setUTCHours(0, 50, 0, 0);
+  cy.log(bookingTime.toString());
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate < bookingTime);
+};
 
-            cy.intercept('GET', new RegExp(`/marketplace/clubs/${booking.clubId}/teetimes\?.*`), {
-                statusCode: 201,
-                body: {
-                },
-              }).as('bookingConfig')
+describe("Book A Tee Time", () => {
+  const currentDate = new Date();
+  const bookingDate = addDays(currentDate, 4);
+  const todaysBooking = bookings.filter((booking) => {
+    return (
+      booking.month ==
+        bookingDate.toLocaleString("default", { month: "long" }) &&
+      booking.day == bookingDate.getDate()
+    );
+  });
 
-            bookingPage.navigateTo(booking.clubId);
-            bookingPage.setDate(booking.month, booking.day);
-            const bookingReview = bookingPage.selectTeeTime(booking.time, booking.numPlayers);
+  if (todaysBooking) {
+    todaysBooking.forEach((booking) => {
+      it(`Book TeeTime for ${booking.time} on ${booking.month} ${booking.day} `, () => {
+        cy.visit("");
+        cy.intercept("GET", "/private_api/sessions").as("loginSuccess");
+        login.login();
+        cy.wait("@loginSuccess");
 
-            bookingReview.confirmBooking();
-        });
+        cy.intercept(
+          "GET",
+          new RegExp(`/marketplace/clubs/${booking.clubId}/teetimes\?.*`),
+          {
+            statusCode: 201,
+            body: {},
+          }
+        ).as("bookingConfig");
+
+        bookingPage.navigateTo(booking.clubId);
+        bookingPage.setDate(booking.month, booking.day);
+        sleepUntilBookngTime();
+        const bookingReview = bookingPage.selectTeeTime(
+          booking.time,
+          booking.numPlayers
+        );
+
+        //bookingReview.confirmBooking();
+      });
     });
-    
+  } else {
+    it("No Bookings to make today", () => {
+      cy.log("No bookings set today");
+    });
+  }
+
+  it("No Bookings to make today", () => {
+    bookings.forEach((booking) => {
+      cy.log(booking.month + " " + booking.day);
+    });
+    cy.log(
+      bookingDate.toLocaleString("default", { month: "long" }) +
+        " " +
+        bookingDate.getDate()
+    );
+
+    cy.log(
+      bookings
+        .filter((booking) => {
+          return booking.month == "May";
+        })
+        .toString()
+    );
+  });
 });
